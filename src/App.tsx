9bc152021f1f126274250f49674095187dcdb4f3
isabelino <icordoba8@misena.edu.co>
2021-06-10 17:10:09 -0500

    Agregar cookies para la firma autor

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useCookies } from "react-cookie";
 import { BrowserRouter as Router, HashRouter } from "react-router-dom";
 import Layout from "./components/Layout";
-
+import { autor } from "../package.json";
 const App = () => {
+  const [cookies, setCookie] = useCookies(["autor"]);
+  useEffect(() => {
+    setCookie("autor", JSON.stringify(autor), { path: "/" });
+  }, [setCookie]);
+
   return (
     <Router>
       {/* <HashRouter> */}
